077cfd94b5e82d168316713df8c119495762f3c1
Trung Doan <trungtrong456@gmail.com>
2014-04-15 17:26:46 +0700

    Functional test now can check user input

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -11,8 +12,33 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # User go to home page url
         self.browser.get('http://localhost:8000')
+
+        # He see page title is 'To-do' and header is also 'To-do'
         self.assertIn('To-do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-do', header_text)
+
+        # He see a text input to input new to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # He type in 'Buy peacock feathers' into the text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When he hits enter, the page update and add a new to-do item
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
