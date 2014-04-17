3bff950ee0f2992e4abf05720cb4bcdae64d707a
Trung Doan <trungtrong456@gmail.com>
2014-04-17 10:55:42 +0700

    new URL, view and template display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,9 +2,6 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-import unittest
-
-
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
@@ -12,6 +9,11 @@ class NewVisitorTest(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User go to home page url
@@ -50,7 +52,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Another user create his own list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feather', page_text)
+        self.assertNotIn('Buy peacock feathers', page_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
