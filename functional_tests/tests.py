2163457b22decfa547b4f6122f1ec1db3639dbde
Trung Doan <trungtrong456@gmail.com>
2014-04-17 13:59:13 +0700

    new Url and view for adding items to existing list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,7 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
@@ -33,18 +34,17 @@ class NewVisitorTest(LiveServerTestCase):
 
         # He type in 'Buy peacock feathers' into the text box
         inputbox.send_keys('Buy peacock feathers')
-
-        # When he hits enter, the page update and add a new to-do item
         inputbox.send_keys(Keys.ENTER)
+
+        # He type in one more item to list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to fly')
+        inputbox.send_keys(Keys.ENTER)
+
         edith_list_url = self.browser.current_url
         self.assertRegexpMatches(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        #self.assertIn('2: Use peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('2: Use peacock feathers to fly')
 
         self.browser.quit()
         self.browser = webdriver.Firefox()
@@ -53,6 +53,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Use peacock feathers to fly', page_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
