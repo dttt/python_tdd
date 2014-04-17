dc3616ac8afd12a79cc822d3f48b2e189a2b61ac
Trung Doan <trungtrong456@gmail.com>
2014-04-17 10:24:44 +0700

    Test many list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -34,11 +34,37 @@ class NewVisitorTest(LiveServerTestCase):
 
         # When he hits enter, the page update and add a new to-do item
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegexpMatches(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
 
         self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         #self.assertIn('2: Use peacock feathers', [row.text for row in rows])
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Another user create his own list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feather', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegexpMatches(francis_list_url, '/lists/.+')
+
+        # Check 2 users's list are not the same
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Francis should not see Edith list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
         self.fail('Finish the test!')
