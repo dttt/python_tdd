f496c860fd97a45b1ed4745669f9e380345f9c4e
Trung Doan <trungtrong456@gmail.com>
2014-04-18 18:04:23 +0700

    Renamed all item input id

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,5 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 
 class FunctionalTest(LiveServerTestCase):
@@ -16,87 +15,5 @@ class FunctionalTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-
-class NewVisitorTest(FunctionalTest):
-
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # User go to home page url
-        self.browser.get(self.live_server_url)
-
-        # He see page title is 'To-do' and header is also 'To-do'
-        self.assertIn('To-do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-do', header_text)
-
-        # He see a text input to input new to-do item
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-
-        # He type in 'Buy peacock feathers' into the text box
-        inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
-
-        # He type in one more item to list
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to fly')
-        inputbox.send_keys(Keys.ENTER)
-
-        edith_list_url = self.browser.current_url
-        self.assertRegexpMatches(edith_list_url, '/lists/.+')
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to fly')
-
-        self.browser.quit()
-        self.browser = webdriver.Firefox()
-
-        # Another user create his own list
-        self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertNotIn('Use peacock feathers to fly', page_text)
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy milk')
-        inputbox.send_keys(Keys.ENTER)
-
-        francis_list_url = self.browser.current_url
-        self.assertRegexpMatches(francis_list_url, '/lists/.+')
-
-        # Check 2 users's list are not the same
-        self.assertNotEqual(francis_list_url, edith_list_url)
-
-        # Francis should not see Edith list
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('Buy milk', page_text)
-
-
-class LayoutAndStylingTest(FunctionalTest):
-
-    def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=5
-        )
-
-        inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=5
-        )
-
-
-class ItemValidationTest(FunctionalTest):
-
-    def test_cannot_add_empty_list_items(self):
-        self.fail('write me!')
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
